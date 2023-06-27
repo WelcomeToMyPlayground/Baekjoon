875d7c5263d43184e6031952356b44643d91f437
WelcomeToMyPlayground <124188377+WelcomeToMyPlayground@users.noreply.github.com>
2023-06-27 19:22:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 220 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		Long A = s.nextLong(), B = s.nextLong(), C = s.nextLong();
+		Long sum = A + B + C;
+		System.out.println(sum);
+
+	}
+
+}
