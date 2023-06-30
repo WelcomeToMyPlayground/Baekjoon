f01436b3f2138f8e4840364881cdbce0f14ba581
WelcomeToMyPlayground <124188377+WelcomeToMyPlayground@users.noreply.github.com>
2023-07-01 01:13:26 +0900

    [Bronze V] Title: A+B - 7, Time: 256 ms, Memory: 18728 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int T = s.nextInt();
+
+		for (int i = 1; i <= T; i++) {
+			int A = s.nextInt();
+			int B = s.nextInt();
+			System.out.println("Case #" + i + ": " + (A + B));
+		}
+	}
+}
