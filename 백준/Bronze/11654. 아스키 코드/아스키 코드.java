5ef946ad4f1cbc9f9c68320a85b7408307ad5d05
WelcomeToMyPlayground <124188377+WelcomeToMyPlayground@users.noreply.github.com>
2023-07-01 18:56:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		char b = a.charAt(0);
+		int c = b;
+		System.out.println(c);
+
+	}
+}
